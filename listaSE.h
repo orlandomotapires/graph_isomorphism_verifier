c206f1714c72db89e45ad334523b920d458f2fd9
orlandomotapires <orlandomota2002@gmail.com>
2023-08-25 15:00:41 -0300

    quase la

--- listaSE.h
+++ listaSE.h
@@ -3,16 +3,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef int tp_item;
+typedef char tp_item;
 
 typedef struct tp_no{
-  struct tp_no **adj;
+  struct tp_no *adj;
   struct tp_no *prox;
+  tp_item info;
 }tp_listase;
 
 typedef struct{
-    tp_listase *lista;
-    char info;
+  tp_listase *lista;
 }graph;
 
 tp_listase *inicializa_listase(){
@@ -24,74 +24,90 @@ int listase_vazia(tp_listase *lista){
   else return 0;
 }
 
-// tp_listase *aloca_listase(){
-//   tp_listase *novo_no;
-//   novo_no = (tp_listase*) malloc(sizeof(tp_listase));
-//   return novo_no;
-// }
+tp_listase *aloca_listase(){
+  tp_listase *novo_no;
+  novo_no = (tp_listase*) malloc(sizeof(tp_listase));
+  return novo_no;
+}
 
-// int insere_listase_no_fim(tp_listase **l, tp_item e){
-//   tp_listase *novo_no, *atu;
-//   novo_no = aloca_listase();
-//   if(novo_no == NULL) return 0;
-//   novo_no->adj = e;
-//   novo_no->prox = NULL;
+tp_listase *procura_elemento_lista_principal(tp_listase *lista, tp_item e){
+  tp_listase *atu;
+  atu = lista;
+  while(atu->prox != NULL){
+    if(atu->info == e){
+      return atu;
+    }
+    atu=atu->prox;
+  }
 
-//   if(listase_vazia(*l)){
-//     *l = novo_no;
-//   } else{
-//     atu = *l;
-//     while(atu->prox != NULL){
-//       atu = atu->prox;
-//     }
-//     atu->prox=novo_no;
-//   }
+  return NULL;
+}
 
-// return 1;
-// }
+tp_listase *procura_elemento_base(tp_listase *lista, int pos){
+  tp_listase *atu;
+  atu = lista;
 
-// void imprime_listase(tp_listase *lista, int i){
-//   tp_listase *atu;
-//   atu = lista;
-//   int c=0;
-//   while(c<i){
-//     printf("%d ", atu->info);
-//     atu=atu->prox;
-//     c++;
-//   }
-// }
+  int c = 0;
+  while(atu->prox != NULL){
+    if(pos == c){
+      return atu;
+    }
+    atu=atu->prox;
+    c++;
+  }
 
-// int tamanho_listase(tp_listase *lista){
-//   int cont = 0;
-//   tp_listase *atu;
-//   atu=lista;
-//   while(atu!=NULL){
-//     cont++;
-//     atu=atu->prox;
-//   }
-//   return cont;
-// }
+  return NULL;
+}
 
-// void desfaz_lista(tp_listase **lista){
-//   tp_listase *atu;
-//   atu = *lista;
-//   while(atu!=NULL){
-//     *lista = atu->prox;
-//     free(atu);
-//   }
-// }
+int insere_listase_no_fim(tp_listase **l, tp_item e){
+  tp_listase *novo_no, *atu;
+  novo_no = aloca_listase();
+  if(novo_no == NULL) return 0;
+  
+  novo_no->info = e;
+  novo_no->adj = procura_elemento_lista_principal(l, e);
+  novo_no->prox = NULL;
 
+  if(listase_vazia(*l)){
+    *l = novo_no;
+  } else{
+    atu = *l;
+    while(atu->prox != NULL){
+      atu = atu->prox;
+    }
+    atu->prox=novo_no;
+  }
 
-// int busca_listase(tp_listase *lista, tp_item e){
-//   tp_listase *atu;
-//   int c=0;
-//   atu = lista;
-//   while((atu!=NULL) && (atu->info!=e)){
-//     atu = atu->prox; 
-//     c++;
-//   }
-//   if(atu == NULL) return 0;
-//   return c;
-// }
+  return 1;
+}
+
+void imprime_listase(tp_listase *lista){
+  tp_listase *atu;
+  atu = lista;
+  
+  printf("%c -> ", atu->info);
+  while(atu->prox != NULL){
+    printf("%c", atu->info);
+    atu = atu -> prox; 
+  }
+}
+
+void imprime_listase_inteira(tp_listase *lista){
+  tp_listase *atu;
+  atu = lista;
+  int c=0;
+  
+  while(atu->prox != NULL){  
+    atu=atu->prox;
+    c++;
+
+    printf("\nIMPRIMINDO A LISTA DA LETRA %c\n", atu->info);
+    if(atu->adj != NULL)
+      printf("\nIMPRIMINDO A LISTA DA LETRA adj %c\n", atu->adj->info);
+    imprime_listase(atu);
+
+  }
+}
+
 
 #endif
--- verifica_isomorfos.c
+++ verifica_isomorfos.c
@@ -4,16 +4,17 @@
 
 const int MAX = 1e2 + 10;
 
-graph g; 
+graph *g; 
 
 int main(){
 
     char linhas[MAX][MAX];
     int vis[MAX];  
 
+    g = aloca_listase();
+
     FILE *arq;
     arq = fopen("adj.txt", "rt");
-
 
     if(arq == NULL){
         printf("Arquivo nao foi aberto!");
@@ -27,13 +28,30 @@ int main(){
         if(fgets(linha, 50, arq)) strcpy(linhas[it++], linha);
     }
 
-    for(int i = 0; i < strlen(linhas); i++){
-        for(int j = 0; j < strlen(linhas[i]); j++){
-            printf("%c ", linhas[i][j]);
-        }
+    for(int i = 0; i < strlen(linhas[0]); i++){
+        insere_listase_no_fim(&g, linhas[0][i]);
+    }  
 
-        printf("\n");
+    for(int i = 1; i < strlen(linhas); i++){
+        for(int j = 0; j < strlen(linhas[i]); j++){
+            if(linhas[i][j] == '1'){              
+                tp_listase *no_base_correto = procura_elemento_base(g, i);
+                //printf("No_base_correto->info: %c", no_base_correto->info);
+                //printf("\n");
+
+                insere_listase_no_fim(no_base_correto, linhas[0][j]);  
+    
+            }
+        }
     }
+
+    imprime_listase_inteira(g);
+
+//   ABCD
+// A 1111
+// B 1011
+// C 1101
+// D 1111
 
     return 0;
 
